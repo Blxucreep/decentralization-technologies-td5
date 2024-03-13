649648740a5c5f415aeab2727d5ab6fff2b1d812
Blxucreep <LoevanLqc@outlook.com>
2024-03-13 04:32:33 +0100

    testing part 2

--- __test__/tests/benorconsensus.test.ts
+++ __test__/tests/benorconsensus.test.ts
@@ -39,7 +39,7 @@ describe("Ben-Or decentralized consensus algorithm", () => {
       await delay(100);
       await closeAllServers(servers);
       servers.splice(0);
-    }, 15000);
+    });
 
     it("Can start 2 healthy nodes and 1 faulty node - 2 pts", async () => {
       const faultyArray = [true, false, false];
@@ -116,7 +116,7 @@ describe("Ben-Or decentralized consensus algorithm", () => {
       }
     });
   });
-  /*
+
   describe("Testing Ben-Or implementation - 16 pt", () => {
     const servers: http.Server[] = [];
 
@@ -488,5 +488,4 @@ describe("Ben-Or decentralized consensus algorithm", () => {
 
     test.todo("Hidden Test - 1 pt");
   });
-  */
 });
--- src/nodes/node.ts
+++ src/nodes/node.ts
@@ -2,6 +2,7 @@ import bodyParser from "body-parser";
 import express from "express";
 import { BASE_NODE_PORT } from "../config";
 import { NodeState, Value } from "../types";
+import e from "express";
 
 export async function node(
   nodeId: number, // the ID of the node
@@ -16,6 +17,13 @@ export async function node(
   node.use(express.json());
   node.use(bodyParser.json());
 
+  const nodeState: NodeState = {
+    killed: false,
+    x: null,
+    decided: null,
+    k: null,
+  };
+
   // TODO implement this
   // this route allows retrieving the current status of the node
   node.get("/status", (req, res) => {
@@ -29,31 +37,51 @@ export async function node(
 
   // TODO implement this
   // this route allows the node to receive messages from other nodes
-  node.post("/message", (req, res) => { });
+  node.post("/message", (req, res) => {
+    const message = req.body;
+
+    nodeState.x = message.x;
+    nodeState.k = message.k;
+
+    res.status(200).send("message received");
+  });
 
   // TODO implement this
   // this route is used to start the consensus algorithm
-  node.get("/start", async (req, res) => { });
+  node.get("/start", async (req, res) => {
+    if (!nodesAreReady()) {
+      res.status(500).send("nodes are not ready yet");
+      return;
+    }
+
+    // the algorithm should set the value of nodeState.x and nodeState.decided
+    nodeState.x = initialValue;
+    nodeState.decided = true;
+    
+    // the algorithm should also set the value of nodeState.k
+    nodeState.k = 0;
+    // the value of nodeState.k should be the round number at which the node decided on a value
+
+    // the algorithm should be non-blocking and should return immediately
+
+    // the algorithm should be started by calling the /start route
+    
+
+    // the algorithm should be stopped by calling the /stop route
+
+    // the algorithm
+  });
 
   // TODO implement this
   // this route is used to stop the consensus algorithm
-  node.get("/stop", async (req, res) => { });
+  node.get("/stop", async (req, res) => {
+    nodeState.killed = true;
+    res.status(200).send("consensus stopped");
+  });
 
   // TODO implement this
   // get the current state of a node
   node.get("/getState", (req, res) => {
-    const nodeState: NodeState = {
-      killed: false,
-      x: null,
-      decided: null,
-      k: null,
-    };
-
-    // check if the node is faulty
-    // if (isFaulty) {
-    //   nodeState.killed = true;
-    // }
-
     res.json(nodeState);
   });
 
